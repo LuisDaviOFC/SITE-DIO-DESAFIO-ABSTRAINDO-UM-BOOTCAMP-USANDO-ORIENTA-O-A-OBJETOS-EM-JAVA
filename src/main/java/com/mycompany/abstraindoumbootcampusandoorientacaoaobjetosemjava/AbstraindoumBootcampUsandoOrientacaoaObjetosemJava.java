a6012a1ed0e738bd5f3db70d239994db2b3ff9ad
Luis Davi Vieira SIlva <85641308+LuisDaviOFC@users.noreply.github.com>
2023-03-21 09:16:33 -0300

    first commit

---
+++ src/main/java/com/mycompany/abstraindoumbootcampusandoorientacaoaobjetosemjava/AbstraindoumBootcampUsandoOrientacaoaObjetosemJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.abstraindoumbootcampusandoorientacaoaobjetosemjava;
+
+/**
+ *
+ * @author LuisDaviVieiraSilva
+ */
+public class AbstraindoumBootcampUsandoOrientacaoaObjetosemJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
